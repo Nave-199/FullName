2198b7955296593698b60e6a5751d2960a55496b
NaVeEN98801 <naveenn199999@gmail.com>
2024-07-25 11:55:16 +0530

    FullName added

--- fullname/src/Fullname.js
+++ fullname/src/Fullname.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Fullname = () => {
-  return <div>Fullname</div>;
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSetFull = (e) => {
+    e.preventDefault();
+    if (firstname !== "" && lastname !== "") {
+      setFullName(firstname + " " + lastname);
+    } else {
+      setFullName("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Full Name Dispaly</h1>
+      <form>
+        <div>
+          <label>First Name:</label>
+          <input
+            type="text"
+            onInput={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>last Name:</label>
+          <input
+            type="text"
+            onInput={(e) => setLastName(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" onClick={(e) => handleSetFull(e)}>
+          Submit
+        </button>
+      </form>
+      {fullName && <div>Full Name: {fullName}</div>}
+    </div>
+  );
 };
 
 export default Fullname;
